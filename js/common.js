924c50a719a3fe9efa846fce22495ba157175cce
v.monasteretskyy <v.monasteretskyy@gmail.com>
2019-12-06 10:21:47 +0200

    home page

---
+++ js/common.js
@@ -0,0 +1,70 @@
+$(document).ready(function () {
+    $('.header__language').click(function () {
+        $(this).children('ul').slideToggle();
+    });
+
+    // reviews sldier
+    $('.reviews-slider').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: false,
+        arrows: false,
+        dots: true,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1,
+                }
+            }
+        ]
+    });
+
+    // Инициализация
+    // Доступ к экземпляру объекта
+    $('.from-date,.to-date').data('datepicker')
+
+    // burger
+    $("div.burger").click(function () {
+        $(this).toggleClass("selected");
+
+        $('.header__menu').toggleClass('showmenu');
+    });
+
+
+
+
+
+    // read more
+    $('.readall span').click(function () {
+        $('.subscribe__textblock p:nth-child(n+3)').fadeIn();
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    $(function () {
+        $('.choose-route select').styler();
+    });
+});
